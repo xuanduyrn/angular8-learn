05558eda8896c84c5add3738cc69a83643823638
htactive-duypx <duypx@htactive.com>
2019-11-18 16:17:42 +0700

    feat: form submit

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartServices } from '../cart.service';
 
 @Component({
@@ -8,11 +9,25 @@ import { CartServices } from '../cart.service';
 
 export class CartComponent implements OnInit {
   items;
+  checkoutForm;
   constructor(
-    private cartServices: CartServices
-  ) {};
-
+    private cartServices: CartServices,
+    private formBuilder: FormBuilder
+  ) {
+    this.items = this.cartServices.getItems();
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    })
+  };
+  
   ngOnInit() {
     this.items = this.cartServices.getItems();
+  }
+
+  onSubmit(customerData) {
+    console.warn('Your order has been submitted', customerData);
+    this.items = this.cartServices.clearCart();
+    this.checkoutForm.reset();
   }
 }

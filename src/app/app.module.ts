54b602db327da09acd22be0ba9a7d2f61d7f1098
htactive-duypx <duypx@htactive.com>
2019-11-18 16:27:02 +0700

    feat: move folder shop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
-import { ShippingComponent } from './shipping/shipping.component';
+import { TopBarComponent } from './shop/top-bar/top-bar.component';
+import { ProductListComponent } from './shop/product-list/product-list.component';
+import { ProductAlertsComponent } from './shop/product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './shop/product-details/product-details.component';
+import { ShippingComponent } from './shop/shipping/shipping.component';
 
-import { CartComponent } from './cart/cart.component';
+import { CartComponent } from './shop/cart/cart.component';
 
 @NgModule({
   declarations: [

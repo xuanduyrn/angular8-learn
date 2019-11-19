89e6ff51a63422a2bfc2e5fb5e7b388b5a9525cc
htactive-duypx <duypx@htactive.com>
2019-11-19 11:27:15 +0700

    feat: rmeove comment router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// Shop
 import { TopBarComponent } from './shop/top-bar/top-bar.component';
 import { ProductListComponent } from './shop/product-list/product-list.component';
 import { ProductAlertsComponent } from './shop/product-alerts/product-alerts.component';
@@ -14,30 +13,23 @@ import { ProductDetailsComponent } from './shop/product-details/product-details.
 import { ShippingComponent } from './shop/shipping/shipping.component';
 import { CartComponent } from './shop/cart/cart.component';
 
-// Hero
 import { HeroesComponent } from './hero/heroes/heroes.component';
 import { HeroDetailComponent } from './hero/hero-detail/hero-detail.component';
-// Message
 import { MessagesComponent } from './messages/messages.component';
-// dashboard
 import { DashBoardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    //shop
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
     CartComponent,
     ShippingComponent,
-    // hero
     HeroesComponent,
     HeroDetailComponent,
-    // Message
     MessagesComponent,
-    // dashboard
     DashBoardComponent
   ],
   imports: [
@@ -47,17 +39,13 @@ import { DashBoardComponent } from './dashboard/dashboard.component';
     AppRoutingModule,
     FormsModule,
     RouterModule.forRoot([
-      // Shop
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
       { path: 'shipping', component: ShippingComponent },
-      // Hero
       { path: 'hero', component: HeroesComponent },
       { path: 'hero/:heroId', component: HeroDetailComponent },
-      // message
       { path: 'message', component: MessagesComponent },
-      // dashboard
       { path: 'dashboard', component: DashBoardComponent}
     ])
   ],
